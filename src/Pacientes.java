8007f9f91303aa74cdc496529b30626359729fb2
guilherme-dis <guilherme.dis@ufu.br>
2021-08-31 23:10:34 -0300

    3_cCompletos
    Fiz até a c

--- src/Pacientes.java
+++ src/Pacientes.java
@@ -1,81 +1,12 @@
 import java.sql.Date;
 
-public class Pacientes {
-    private String nome;
-    private String cpf;
-    private String rg;
-    private byte estadoCivil;
+public class Pacientes extends Pessoas{
     private Date dataUltimaConsulta;
     private String sexo;
     private Date dateNascimento;
     private Date dataCadastro;
 
-    // NOME
-    public String getNome() {
-        return nome;
-    }
-
-    public boolean setNome(String nome) {
-        if (nome.length() > 0) {
-            this.nome = nome;
-            return true;
-        } else
-            return false;
-
-    }
-
-    // CPF
-    public String getCpf() {
-        return cpf;
-    }
-
-    public boolean setCpf(String cpf) {
-        if (ValidaCPF.isCPF(cpf)) {
-            this.cpf = cpf;
-            return true;
-        } else
-            return false;
-    }
-
-    // RG
-    public String getRg() {
-        return rg;
-    }
-
-    public boolean setRg(String rg) {
-        if (rg.length() != 8) {
-            return false;
-        }
-        this.rg = rg;
-        return true;
-    }
-
-    // ESTADO CIVIL
-    public String getEstadoCivil() {
-        if (this.estadoCivil == 0) {
-            return "solteiro";
-        } else if (this.estadoCivil == 1) {
-            return "casado";
-        }
-        return "divorciado";
-    }
-
-    public boolean setEstadoCivil(String estadoCivil) {
-        if (estadoCivil == "solteiro") {
-            this.estadoCivil = 0;
-            return true;
-        }
-        if (estadoCivil == "casado") {
-            this.estadoCivil = 1;
-            return true;
-        }
-        if (estadoCivil == "divorciado") {
-            this.estadoCivil = 2;
-            return true;
-        }
-
-        return false;
-    }
+    
 
     // DATA DA ÚLTIMA CONSULTA
     public Date getDataUltimaConsulta() {
@@ -118,13 +49,15 @@ public class Pacientes {
     }
     //MÉTODOS
 
-    public Pacientes(String nome, String cpf, byte estadoCivil, String sexo, Date dateNascimento, Date dataCadastro) {
-        this.nome = nome;
-        this.cpf = cpf;
-        this.estadoCivil = estadoCivil;
+    public Pacientes(String nome, String cpf, String rg, byte estadoCivil, Date dataUltimaConsulta, String sexo,
+            Date dateNascimento, Date dataCadastro) {
+        super(nome, cpf, rg, estadoCivil);
+        this.dataUltimaConsulta = dataUltimaConsulta;
         this.sexo = sexo;
         this.dateNascimento = dateNascimento;
         this.dataCadastro = dataCadastro;
     }
+
+    
 
 }
