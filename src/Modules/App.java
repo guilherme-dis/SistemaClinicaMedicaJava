6896f126585c97d204f26ed88f216b5679359859
Guilherme Dias <guilherme.dis@ufu.br>
2021-10-12 12:53:30 -0300

    Parte 2 do trabalho finalizado.

--- src/Modules/App.java
+++ src/Modules/App.java
@@ -1,5 +1,4 @@
 package Modules;
-//TODO IMPLEMENTAR O ENDEREÇO
 
 import DataClass.*;
 
@@ -9,37 +8,82 @@ import java.util.Date;
 public class App implements Serializable {
 
     public static void main(String[] args) {
-        //TODO aprender a trabalhar ocm o date.
         try {
             Date date = new Date();
-            startData();
+            //startData(); //Metodo criado para inicializar tudo de uma vez só
             System.out.println("Bem vindo ao sistema!");
 
 
+            //Cadastro dos exames
+            DadosExames.inicializaExames();
+            DadosExames.cadastrar(new Exames("Hemograma", 20, 0));
+            DadosExames.cadastrar(new Exames("Covid", 160, 0));
+            DadosExames.cadastrar(new Exames("Glicemia", 40, 0));
+            DadosExames.cadastrar(new Exames("Colesterol", 30, 0));
+            DadosExames.cadastrar(new Exames("Ureia", 20, 0));
+            DadosExames.cadastrar(new Exames("Transaminases", 50, 0));
             DadosExames.listar();
-            System.out.println();
+
+            //Cadastro dos plano de saúde
+            DadosPlanosDeSaude.inicializaPlanoDeSaude();
+            DadosPlanosDeSaude.cadastrar(new PlanoDeSaude("Votorantim", "55555"));
+            DadosPlanosDeSaude.cadastrar(new PlanoDeSaude("SulAmérica", "55555"));
+            DadosPlanosDeSaude.cadastrar(new PlanoDeSaude("NotreDame Intermédica", "55555"));
+            DadosPlanosDeSaude.cadastrar(new PlanoDeSaude("Prevent Senior", "55555"));
+            DadosPlanosDeSaude.cadastrar(new PlanoDeSaude("Assim Saúde", "55555"));
             DadosPlanosDeSaude.listar();
-            System.out.println();
+
+            //Cadastro de pacientes
+            DadosPacientes.inicializaPacientes();
+            DadosPacientes.cadastrar(new PacienteComPlanoDeSaude("Arthur", "90718598008", "36939488", "casado", new Endereco("Graziel ferreira amorin", "casa", "bela vista", "itau", "780", "37975000"), "Masculino", date, date, DadosPlanosDeSaude.buscar("Votorantim"), "", "", date));
+            DadosPacientes.cadastrar(new PacienteSemPlanoDeSaude("Guilherme Dias Cardoso Silva", "70122924665", "19215568", "solteiro", new Endereco("waldomiro caetano machado", "casa", "nova california", "passos", "81", "37904570"), "masculino", date, date, 20));
             DadosPacientes.listar();
-            System.out.println();
+
+            //Cadastro de funcionarios.
+            DadosFuncionarios.inicializaFuncionarios();
+            DadosFuncionarios.cadastrar(new OutrosFuncionarios("Isadora Maria", "44225173097", "55555555", "casado", new Endereco("Graziel ferreira amorin", "casa", "bela vista", "itau", "780", "37975000"), "isadora", "isa123", "1111", date, 1100, "Secretaria"));
+            DadosFuncionarios.cadastrar(new Medicos("Carlos", "26776897077", "55555555", "casado", new Endereco("Graziel ferreira amorin", "casa", "bela vista", "itau", "780", "37975000"), "carlin", "carlin777", "1111", date, 7000, "7777", new String[]{"sangue", "Hepatite"}, 10, 100, new PlanoDeSaude[]{DadosPlanosDeSaude.buscar("Votorantim"), DadosPlanosDeSaude.buscar("Prevent Senior")}, 50));
             DadosFuncionarios.listar();
-            System.out.println();
+
+
+            //Cadastro consulta
+            String[] exames1 = new String[]{"Hemograma", "Glicemia", "Colesterol", "Transaminases"};
+            String[] exames2 = new String[]{"Hemograma", "Colesterol", "Transaminases"};
+
+            DadosConsulta.cadastrar(new Consulta(date, "Dipirona", "Voltar em uma semana", "Dipirona de 8 em 8 horas", DadosPacientes.buscar("70122924665"), DadosFuncionarios.buscar("26776897077"), exames1, DadosFuncionarios.buscar("44225173097")));
             DadosConsulta.listar();
-            System.out.println();
+
+            //Simulando 3 consultas com o mesmo paciente.
+            System.out.println("\nValor que o paciente vai pagar sem plano de saude vai pagar " + DadosConsulta.buscar("70122924665").realizarConsulta());
+            System.out.println("\nValor que o paciente vai pagar sem plano de saude vai pagar " + DadosConsulta.buscar("70122924665").realizarConsulta());
+            System.out.println("\nValor que o paciente vai pagar sem plano de saude vai pagar " + DadosConsulta.buscar("70122924665").realizarConsulta());
+
+            //Valor a ser pago de gratificação caso o limite de consultas seja atingido.
+            OutrosFuncionarios.setValorASerPagoDeGratificacao(100);
+
+            //A cada 50 consultas, os funcionarios ganham o valor de gratificação
+            Consulta.setLimiteConsultas(50);
 
 
+            //Calculando o salário do funcionario
+            System.out.println(DadosFuncionarios.buscar("44225173097").calcularSalario());
 
+            //Calculando o salário do médico
+            System.out.println(DadosFuncionarios.buscar("26776897077").calcularSalario());
 
+            //3.d, zerar o número de consultas do mes
+            Consulta.resetConsultas();
 
-
-
-
+            //Utilizando os métodos abstratos criados para o exercício 5 c
+            DadosFuncionarios.buscar("44225173097").resetaInfo();
+            DadosFuncionarios.buscar("26776897077").resetaInfo();
 
 
         } catch (RuntimeException e) {
             System.err.println(e.getMessage());
             System.exit(0);
         }
+        //Esse método salva todos os dados.
         saveData();
     }
 
